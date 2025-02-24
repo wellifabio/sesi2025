743e2b0e3211262bcdb466156ff53bd0472368cb
wellifabio <wellifabio@hotmail.com>
2025-02-24 11:29:48 -0300

    aula 04

--- ds/3des/01-pfe1/aula04/modal-storage/script.js
+++ ds/3des/01-pfe1/aula04/modal-storage/script.js
@@ -1,8 +1,9 @@
 const atletas = JSON.parse(window.localStorage.getItem('atletas'));
-if(atletas === null) {
+if (atletas === undefined || atletas === null) {
     window.localStorage.setItem('atletas', JSON.stringify([]));
     atletas = [];
 }
+
 const main = document.querySelector('main');
 atletas.forEach(atleta => {
     const card = document.createElement('div');
@@ -19,7 +20,7 @@ atletas.forEach(atleta => {
 });
 
 function excluir(id) {
-    if(confirm('Realmente quer excluir o card '+id+' ?')) {
+    if (confirm('Realmente quer excluir o card ' + id + ' ?')) {
         const indice = atletas.findIndex(atleta => atleta.id === id);
         atletas.splice(indice, 1);
         window.localStorage.setItem('atletas', JSON.stringify(atletas));
